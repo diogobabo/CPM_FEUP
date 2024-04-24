6bfd7eb5528d331837e90e55eb70592559af1c24
diogobabo <diogo.babo@gmail.com>
2024-04-24 03:26:10 +0100

    fix

--- TicketBuy/app/src/main/java/com/feup/cpm/ticketbuy/MainActivity.kt
+++ TicketBuy/app/src/main/java/com/feup/cpm/ticketbuy/MainActivity.kt
@@ -2,7 +2,7 @@ package com.feup.cpm.ticketbuy
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -28,24 +28,45 @@ class MainActivity : AppCompatActivity()  {
         // Load customer data from local storage
         customer = controller.getLocalCustomer(this)
         println("Customer: $customer")
+        if(customer != null) {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+            setSupportActionBar(binding.appBarMain.toolbar)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+            val drawerLayout: DrawerLayout = binding.drawerLayout
+            val navView: NavigationView = binding.navView
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.nav_home, R.id.nav_cafeteria, R.id.nav_tickets, R.id.nav_register, R.id.nav_cafeteria, R.id.nav_transactions
+                ), drawerLayout
+            )
+            setupActionBarWithNavController(navController, appBarConfiguration)
 
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_cafeteria, R.id.nav_tickets, R.id.nav_register
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            // Get the navigation header view
+            val headerView = navView.getHeaderView(0)
+
+            // Update views in the header
+            val userNameTextView: TextView = headerView.findViewById(R.id.userNameTextView)
+            userNameTextView.text = customer!!.name
+
+            val userEmailTextView: TextView = headerView.findViewById(R.id.userEmailTextView)
+            userEmailTextView.text = customer!!.nif
+
+            navView.setupWithNavController(navController)
+        }
+        else {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            // Redirect to register activity
+
+            setContentView(binding.root)
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
+            navController.navigate(R.id.nav_register)
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
